19f5dc4679b269afd0d0b782f27a71a8188f14d4
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-20 11:27:45 -0500

    abstract classes

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -3,21 +3,21 @@ package shapes;
 import java.awt.*;
 
 // step 5:
-public class Circle extends GraphicObject{
+public  class Circle extends GraphicObject{
     // defining Circle's properties
 // step 5a:
     
     public static void main(String[] args) {
         // instantiate Circle (create a new instance of Circle)
     // step 6a:
-
+        GraphicObject round = new Circle();
 
         // apply correct properties:
     // step 6b:
 
         // call the abstract methods:
     // step 6c:
-
+        round.draw();
 
         // call regular methods from the parent class
 
@@ -30,7 +30,21 @@ public class Circle extends GraphicObject{
     // OR
     // you can make this subclass (Circle) abstract
 // step 5c:
+    void draw(){
+        // this is where the implementation goes
+        // this is where the actual action will be defined
+        System.out.println("draw a circle by making a round line without" +
+                                   " any corners");
+    };
 
+    void resize(){
+        System.out.println("Let's make the circle smaller");
+    }
+
+    void fillColor(){
+        System.out.println("let's make the circle red");
+    }
     
     
+
 }
--- src/shapes/GraphicObject.java
+++ src/shapes/GraphicObject.java
@@ -6,14 +6,24 @@ abstract class GraphicObject {
     // subclasses
     // define a shape's starting position using x and y
  // step 4a:
+    int x, y;
     
     //non-abstract (regular) method that can be implemented the same no
     // matter what the subclass is
 // step 4b:
+    void moveTo(int newX, int newY){
+        System.out.printf("this shape's new coordinates are: %s, %s",
+                newX, newY);
+    }
 
     
     // abstract methods, that can be used by every subclass of
     // GraphicObject, but will be implemented differently
 // step 4c:
+    // draw , resize, fillColor
+    
+    abstract void draw();
+    abstract void resize();
+    abstract void fillColor();
 
 }
