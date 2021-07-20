6c421ae2d62dc637d7dfd2ef13c811aeb8e61a97
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-20 00:09:56 -0500

    first commit

---
+++ src/shapes/GraphicObject.java
@@ -0,0 +1,10 @@
+package shapes;
+
+abstract class GraphicObject {
+    int x,y;
+    void moveTo(int newX, int newY) {
+//        ...
+    }
+    abstract void draw();
+    abstract void resize();
+}
